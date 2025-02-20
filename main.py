e9cd8a3770b76f4afb62d0ad36e088bd75f57b38
Jules Gottraux <jules.gottraux@epfl.ch>
2025-02-20 10:28:32 +0100

    Improve code structure

--- main.py
+++ main.py
@@ -7,105 +7,89 @@ def roll_dice() -> int:
     return random.randint(1, 6)
 
 
+class DiceCombinationChecker:
+    up_to_four_dice_combinations: dict[int, list[tuple[int]]] = {
+        50: [(2, 3), (5,)],
+        100: [(1,)],
+        200: [(2, 2, 2)],
+        300: [(3, 3, 3)],
+        400: [(4, 4, 4), (2, 2, 2, 2)],
+        500: [(5, 5, 5)],
+        600: [(6, 6, 6)],
+        600: [(3, 3, 3, 3)],
+        800: [(4, 4, 4, 4)],
+        1000: [(1, 1, 1), (5, 5, 5, 5)],
+        1200: [(6, 6, 6, 6)],
+        2000: [(1, 1, 1, 1)],
+        # Ignore 5 dice combinations
+    }
 
-all_combinations: dict[int, list[tuple[int]]] = {
-    50: [(2, 3), (5,)],
-    100: [(1,)],
-    200: [(2, 2, 2)],
-    300: [(3, 3, 3)],
-    400: [(4, 4, 4), (2, 2, 2, 2)],
-    500: [(5, 5, 5)],
-    600: [(6, 6, 6)],
-    600: [(3, 3, 3, 3)],
-    800: [(4, 4, 4, 4)],
-    1000: [(1, 1, 1), (5, 5, 5, 5)],
-    1200: [(6, 6, 6, 6)],
-    2000: [(1, 1, 1, 1)],
-    # Ignore 5 dice combinations
-}
+    def __init__(self):
+        pass
 
+    def match(self, score: int, dice: list[int]) -> bool:
+        """
+            Test whether some dice combination corresponds to the given score. The match has to be exact.
 
-def test(score, dice: list):
-    """
-        Test whether some dice combination can be interpreted as a score given the rules
-    """
-    dice = tuple(sorted(dice))
-    if score in all_combinations and any(x == dice for x in all_combinations[score]):
-        # print('Yes', score, dice)
-        return True
-    else:
-        # Recurse
-        recursive_steps = [([1], 100), ([5], 50), ([2, 3], 50)]  # Taking the usuals with their points
-        # recursive_steps += [([2], 0), ([3], 0), ([4], 0), ([5], 0), ([6], 0)]  # Removing useless dice
-        # recursive_steps += [([1], 0), ([5], 0)]  # Removing dice ignoring their points
-        for to_remove, current_score in recursive_steps:
-            if all(i in dice for i in to_remove):
-                current_dice = list(dice)
-                for i in to_remove:
-                    current_dice.remove(i)
-                if test(score - current_score, current_dice):
-                    # print(f"Removing {to_remove} success")
-                    return True
-                else:
-                    # print(f"Removing {to_remove} failed")
-                    pass
-        
-        return False
+            For example:
+                - 300 for [1, 1, 1] returns True
+                - 1000 for [1, 1, 1] returns True
+                - 1000 for [1, 1, 1, 6] returns False
+                - 150 for [2, 3, 5, 5] return True
+        """
+        dice = tuple(sorted(dice))
+        if score in self.up_to_four_dice_combinations and any(x == dice for x in self.up_to_four_dice_combinations[score]):
+            return True
+        else:
+            # Take some points away and recurse
+            recursive_steps = [([1], 100), ([5], 50), ([2, 3], 50)]
+            for to_remove, current_score in recursive_steps:
+                if all(i in dice for i in to_remove):
+                    current_dice = list(dice)
+                    for i in to_remove:
+                        current_dice.remove(i)
+                    if self.match(score - current_score, current_dice):
+                        # print(f"Removing {to_remove} success")
+                        return True
+                    else:
+                        # print(f"Removing {to_remove} failed")
+                        pass
+            
+            return False
 
+    def combine_and_match(self, available_dice, chosen_score, chosen_dice) -> int:
+        chosen_score = int(chosen_score)
+        chosen_dice = [int(x) for x in chosen_dice]
 
-class GameState:
-    current_score = 0
-    available_dice: list[int] = None
-
-    def __init__(self) -> None:
-        self.new_dice()
-    
-    def new_dice(self) -> None:
-        self.available_dice = [roll_dice() for i in range(5)]
-    
-    def roll(self) -> None:
-        self.available_dice = [roll_dice() for i in range(len(self.available_dice))]
-    
-    def keep(self, descr: str):
-        score, *chosen_dice = descr.split()
-        score = int(score)
-
-        if score == 1500:
-            # Separate case
-            dice_set = set(self.available_dice)
+        if chosen_score == 1500:
+            # Separate case to be able to just enter 1500 without specifying the dice
+            dice_set = set(available_dice)
             elem1 = list(dice_set)[0]
-            num_elem1 = len([x for x in self.available_dice if x == elem1])
+            num_elem1 = len([x for x in available_dice if x == elem1])
             full_house = len(dice_set) == 2 and (num_elem1 == 2 or num_elem1 == 3)
             if full_house:
                 print('Full House !!!')
             grande_suite = dice_set == {1, 2, 3, 4, 5} or dice_set == {2, 3, 4, 5, 6}
             if grande_suite:
                 print('Grande Suite !!!')
-            assert full_house or grande_suite, f"To take 1500 points you must have either a full house or a grande suite. You had '{self.available_dice}'"
+            assert full_house or grande_suite, f"To take 1500 points you must have either a full house or a grande suite. You had '{available_dice}'"
 
-            self.available_dice = []
-            self.current_score += 1500
+            return 5
         else:
             assert len(chosen_dice) > 0, "You must specify how you keep the dice."
-            chosen_dice = [int(x) for x in chosen_dice]
 
-            dice_sets = [self.available_dice] + self.add_dice_combination(self.available_dice)
+            dice_sets = [available_dice] + self.add_dice_combination(available_dice)
 
             found = False
             for current_dice in dice_sets:
-                if set(chosen_dice).issubset(current_dice) and test(score, chosen_dice):
-                    self.available_dice = self.available_dice[len(chosen_dice):]
-                    self.current_score += score
+                if set(chosen_dice).issubset(current_dice) and self.match(chosen_score, chosen_dice):
                     found = True
                     break
-            if not found:
-                raise RuntimeError(f"You tried to keep a score of {score} by keeping {chosen_dice} with a roll of {self.available_dice}. This is not allowed.")
-        
-        # Main pleine
-        if len(self.available_dice) == 0:
-            self.new_dice()
-        else: 
-            self.roll()
+            if found:
+                return len(chosen_dice)
+            else:
+                raise RuntimeError(f"You tried to keep a score of {chosen_score} by keeping {chosen_dice} with a roll of {available_dice}. This is not allowed.")
+
             
     def add_dice_combination(self, dice_combination) -> list[list[int]]:
         added_combinations = []
@@ -126,6 +110,33 @@ class GameState:
                 added_combinations.extend(self.add_dice_combination(new_combination))
         
         return added_combinations
+
+
+class GameState:
+    current_score = 0
+    available_dice: list[int] = None
+
+    def __init__(self) -> None:
+        self.new_dice()
+        self.dice_checker = DiceCombinationChecker()
+    
+    def new_dice(self) -> None:
+        self.available_dice = [roll_dice() for i in range(5)]
+    
+    def roll(self) -> None:
+        self.available_dice = [roll_dice() for i in range(len(self.available_dice))]
+    
+    def keep(self, chosen_score: int, chosen_dice: list[int]):
+        num_used_dice = self.dice_checker.combine_and_match(self.available_dice, chosen_score, chosen_dice)
+
+        self.available_dice = self.available_dice[num_used_dice:]
+        self.current_score += chosen_score
+
+        # Main pleine
+        if len(self.available_dice) == 0:
+            self.new_dice()
+        else: 
+            self.roll()
 
 
 
@@ -154,7 +165,10 @@ def main():
                 stop = False
 
             if user_input:
-                game.keep(user_input)
+                chosen_score, *chosen_dice = user_input.split()
+                chosen_score = int(chosen_score)
+                chosen_dice = [int(x) for x in chosen_dice]
+                game.keep(chosen_score, chosen_dice)
 
             if stop:
                 if not user_input:
