e952b5e8051366e46857d1412f494a3d58b24d9b
Jules Gottraux <jules.gottraux@epfl.ch>
2025-02-20 12:12:55 +0100

    Change to multiset, last bug fixed and better code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import random
 
-from collections import defaultdict
+from multiset import Multiset
 
 def roll_dice() -> int:
     """Simulate rolling a single die with a specified number of sides."""
@@ -10,26 +10,35 @@ def roll_dice() -> int:
 
 
 class DiceCombinationChecker:
-    up_to_four_dice_combinations: dict[int, list[tuple[int]]] = {
-        50: [(2, 3), (5,)],
-        100: [(1,)],
-        200: [(2, 2, 2)],
-        300: [(3, 3, 3)],
-        400: [(4, 4, 4), (2, 2, 2, 2)],
-        500: [(5, 5, 5)],
-        600: [(6, 6, 6)],
-        600: [(3, 3, 3, 3)],
-        800: [(4, 4, 4, 4)],
-        1000: [(1, 1, 1), (5, 5, 5, 5)],
-        1200: [(6, 6, 6, 6)],
-        2000: [(1, 1, 1, 1)],
+    up_to_four_dice_combinations: dict[int, list[Multiset[int]]] = {
+        50: [Multiset([2, 3]), Multiset([5,])],
+        100: [Multiset([1,])],
+        200: [Multiset([2, 2, 2])],
+        300: [Multiset([3, 3, 3])],
+        400: [Multiset([4, 4, 4]), Multiset([2, 2, 2, 2])],
+        500: [Multiset([5, 5, 5])],
+        600: [Multiset([6, 6, 6])],
+        600: [Multiset([3, 3, 3, 3])],
+        800: [Multiset([4, 4, 4, 4])],
+        1000: [Multiset([1, 1, 1]), Multiset([5, 5, 5, 5])],
+        1200: [Multiset([6, 6, 6, 6])],
+        2000: [Multiset([1, 1, 1, 1])],
         # Ignore 5 dice combinations
     }
 
     def __init__(self):
         pass
 
-    def match(self, score: int, dice: list[int]) -> bool:
+    def is_full_house(self, dice: Multiset[int]) -> bool:
+        """
+            If and only if each element's multiplicity is 2 or 3
+        """
+        return all(2 <= dice[elem] <= 3 for elem in dice.distinct_elements())
+    
+    def is_grande_suite(self, dice: Multiset[int]) -> bool:
+        return dice == {1, 2, 3, 4, 5} or dice == {2, 3, 4, 5, 6}
+
+    def match(self, score: int, dice: Multiset[int]) -> bool:
         """
             Test whether some dice combination corresponds to the given score. The match has to be exact.
 
@@ -39,18 +48,14 @@ class DiceCombinationChecker:
                 - 1000 for [1, 1, 1, 6] returns False
                 - 150 for [2, 3, 5, 5] return True
         """
-        dice = tuple(sorted(dice))
         if score in self.up_to_four_dice_combinations and any(x == dice for x in self.up_to_four_dice_combinations[score]):
             return True
         else:
             # Take some points away and recurse
-            recursive_steps = [([1], 100), ([5], 50), ([2, 3], 50)]
+            recursive_steps = [(set([1]), 100), (set([5]), 50), (set([2, 3]), 50)]
             for to_remove, current_score in recursive_steps:
-                if all(i in dice for i in to_remove):
-                    current_dice = list(dice)
-                    for i in to_remove:
-                        current_dice.remove(i)
-                    if self.match(score - current_score, current_dice):
+                if to_remove.issubset(dice):
+                    if self.match(score - current_score, dice - to_remove):
                         # print(f"Removing {to_remove} success")
                         return True
                     else:
@@ -59,19 +64,20 @@ class DiceCombinationChecker:
             
             return False
 
-    def create_combinations_and_match(self, available_dice, chosen_score, chosen_dice) -> int:
-        chosen_score = int(chosen_score)
-        chosen_dice = [int(x) for x in chosen_dice]
+    def create_combinations_and_match(self, available_dice: Multiset[int], chosen_score: int, chosen_dice: Multiset[int]) -> int:
+        """
+            Check whether the move chosen by the player, that is choosing to keep this score with these dice, is valid.
+            For a grande suite or a full house, chosen_dice can be empty.
+
+            Return the number of dice used by the player for the validated move.
+        """
 
         if chosen_score == 1500:
             # Separate case to be able to just enter 1500 without specifying the dice
-            dice_set = set(available_dice)
-            elem1 = list(dice_set)[0]
-            num_elem1 = len([x for x in available_dice if x == elem1])
-            full_house = len(dice_set) == 2 and (num_elem1 == 2 or num_elem1 == 3)
+            full_house = self.is_full_house(available_dice)
             if full_house:
                 print('Full House !!!')
-            grande_suite = dice_set == {1, 2, 3, 4, 5} or dice_set == {2, 3, 4, 5, 6}
+            grande_suite = self.is_grande_suite(available_dice)
             if grande_suite:
                 print('Grande Suite !!!')
             assert full_house or grande_suite, f"To take 1500 points you must have either a full house or a grande suite. You had '{available_dice}'"
@@ -80,11 +86,11 @@ class DiceCombinationChecker:
         else:
             assert len(chosen_dice) > 0, "You must specify how you keep the dice."
 
-            dice_sets = self.get_all_dice_combination(available_dice)
+            dice_combinations: list[Multiset[int]] = self.get_all_dice_combination(available_dice)
 
             found = False
-            for current_dice in dice_sets:
-                if set(chosen_dice).issubset(current_dice) and self.match(chosen_score, chosen_dice):
+            for current_dice in dice_combinations:
+                if chosen_dice.issubset(current_dice) and self.match(chosen_score, chosen_dice):
                     found = True
                     break
             if found:
@@ -92,74 +98,52 @@ class DiceCombinationChecker:
             else:
                 raise RuntimeError(f"You tried to keep a score of {chosen_score} by keeping {chosen_dice} with a roll of {available_dice}. This is not allowed.")
 
-    # def get_available_actions(self, available_dice):
-    #     actions: dict[int, list[list[int]]] = defaultdict(list)
-    #     data: tuple[int, list[int], list[int]] = []
-
-    #     for current_dice in self.get_all_dice_combination(available_dice):
-    #         for score, combinations in self.up_to_four_dice_combinations.items():
-    #             for combination in combinations:
-    #                 if combination
-            
-    def get_all_dice_combination(self, initial_dice) -> list[list[int]]:
+    def get_all_dice_combination(self, initial_dice: Multiset[int]) -> list[Multiset[int]]:
         added_combinations = [initial_dice]
-        if (2 in initial_dice and 3 in initial_dice):
-            new_combination = initial_dice.copy()
-            new_combination.remove(2)
-            new_combination.remove(3)
-            new_combination.append(5)
-            added_combinations.append(new_combination)
-            added_combinations.extend(self.get_all_dice_combination(new_combination))
-        elif 5 in initial_dice:
-            new_combination = initial_dice.copy()
-            new_combination.remove(5)
-            if 5 in new_combination:
-                new_combination.remove(5)
-                new_combination.append(1)
-                added_combinations.append(new_combination)
-                added_combinations.extend(self.get_all_dice_combination(new_combination))
+        if {2, 3}.issubset(initial_dice):
+            new_dice = initial_dice - {2, 3}
+            new_dice.add(5)
+            added_combinations.append(new_dice)
+            added_combinations.extend(self.get_all_dice_combination(new_dice))
+        elif Multiset([5, 5]).issubset(initial_dice):
+            new_dice = initial_dice - Multiset([5, 5])
+            new_dice.add(1)
+            added_combinations.append(new_dice)
+            added_combinations.extend(self.get_all_dice_combination(new_dice))
         
         return added_combinations
 
 
 class GameState:
     current_score = 0
-    available_dice: list[int] = None
+    available_dice: Multiset[int] = None
 
     def __init__(self) -> None:
-        self.new_dice()
+        self.roll_new_dice(5)
         self.dice_checker = DiceCombinationChecker()
     
-    def new_dice(self) -> None:
-        self.available_dice = [roll_dice() for i in range(5)]
+    def roll_new_dice(self, num_dice: int) -> None:
+        self.available_dice = Multiset([roll_dice() for i in range(num_dice)])
     
-    def roll(self) -> None:
-        self.available_dice = [roll_dice() for i in range(len(self.available_dice))]
-    
-    def keep(self, chosen_score: int, chosen_dice: list[int]):
+    def keep(self, chosen_score: int, chosen_dice: Multiset[int]):
         num_used_dice = self.dice_checker.create_combinations_and_match(self.available_dice, chosen_score, chosen_dice)
-
-        self.available_dice = self.available_dice[num_used_dice:]
+        if len(self.available_dice) == num_used_dice:
+            # Main pleine
+            self.roll_new_dice(5)
+        else:
+            self.roll_new_dice(len(self.available_dice) - num_used_dice)
         self.current_score += chosen_score
-
-        # Main pleine
-        if len(self.available_dice) == 0:
-            self.new_dice()
-        else: 
-            self.roll()
-
-
-
 
     def show(self):
         print(f'''
         Current score is {self.current_score:d}
-        Available dice: {self.available_dice}
+        Available dice: {sorted(self.available_dice)}
         ''')
 
-    
+
 def main():
     game = GameState()
+    game.available_dice = Multiset([1,2,3,5,6])
     game.show()
 
     while True:
@@ -177,7 +161,7 @@ def main():
             if user_input:
                 chosen_score, *chosen_dice = user_input.split()
                 chosen_score = int(chosen_score)
-                chosen_dice = [int(x) for x in chosen_dice]
+                chosen_dice = Multiset([int(x) for x in chosen_dice])
                 game.keep(chosen_score, chosen_dice)
 
             if stop:
@@ -194,7 +178,7 @@ def main():
                     exit()
 
             game.show()
-            print('\n\n\n')
+            print()
         except KeyboardInterrupt:
             exit()
         except EOFError:
